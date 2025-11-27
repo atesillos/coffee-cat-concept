8fcaaf997d807478d4e345c218a141dcc6a84974
atesillos <atesillos7@gmail.com>
2025-11-27 12:41:01 -0600

    making event

--- src/CoffeeCat.java
+++ src/CoffeeCat.java
@@ -1,3 +1,6 @@
+import com.ProbTable;
+import java.util.Scanner;
+
 public class CoffeeCat {
     /*
     This is the class that will run the "main". Everything will be built here
@@ -13,8 +16,47 @@ public class CoffeeCat {
     public static void main(String[] args) {
         // Start of code
         System.out.println("Hello, cat!");
+        int DAY_COUNT = 0;
 
+        ProbTable customerProbTable = new ProbTable();
 
+        // User I/O Scanner 
+        @SuppressWarnings("resource")
+        Scanner scan = new Scanner(System.in);
+
+        Boolean input = false;
+        String userIO;
+        while(!input) {
+            input = false;
+            System.out.println(
+                "Current day: " + DAY_COUNT + "\n" +
+                "Enter 'Order' to receive order \n"+
+                "Enter 'N' to advance next day"
+            );
+            userIO = scan.nextLine();
+            
+            switch (userIO) {
+                case "N":
+                    System.out.println("Next Day: " + DAY_COUNT);
+                    System.out.println("Difficulty: " + customerProbTable.getPercent(DAY_COUNT));
+                    DAY_COUNT++;
+                    break;
+                case "Order":
+                    System.out.println("Get order");
+                    break;
+                default:
+                    System.out.println("ERROR: Input not recognized, try again.");
+                    break;
+            } // Switch
+        } // While
+    } // Main
+
+    void orderEvent() {
+        @SuppressWarnings("resource")
+        Scanner scanOrder = new Scanner(System.in);
+        System.out.println("Make the order");
+        String orderInput = scanOrder.nextLine();
+        if(orderInput.equals("PERFECT"))
+            System.out.println("ORDER MADE");
     }
-
-}
+} // Class
